5870a93b1335618b71ee845527a623db2853859b
szaker1990 <szaker1990@github.com>
2020-08-24 14:11:10 +0200

    collection component and style

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import { Route } from 'react-router-dom'
+import {Route, Switch} from 'react-router-dom'
 import './App.css';
 import Homepage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.components";
 
 function App() {
-  return (
-    <div>
-      <Route exact path="/" component={Homepage}/>
-    </div>
-  );
+    return (
+        <div>
+            <Switch>
+                <Route exact path="/" component={Homepage}/>
+                <Route path={"/shop"} component={ShopPage}/>
+            </Switch>
+        </div>
+    );
 }
+
 export default App;
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -7,5 +7,4 @@ const Homepage = () => (
         <Directory/>
     </div>
 )
-
 export default Homepage;
