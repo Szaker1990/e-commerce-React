387cb3a60a6c2af7ed37fe92892b187e6f1d13e2
szaker1990 <szaker1990@github.com>
2020-08-25 15:38:49 +0200

    sign in sign out auth

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Homepage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.components";
 import Header from "./components/header/header.component";
 import SignInSignUp from "./pages/sign-in/sign-in-sign-up.component";
-import { auth } from "./firebase/firebase.utils";
+import { auth,createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends Component {
     constructor() {
@@ -17,11 +17,24 @@ class App extends Component {
     unsubscribefromAuth = null
 
     componentDidMount() {
-       this.unsubscribefromAuth = auth.onAuthStateChanged( user =>{
-            this.setState({currentUser: user})
+       this.unsubscribefromAuth = auth.onAuthStateChanged( async userAuth =>{
+            if(userAuth) {
+                const userRef = await createUserProfileDocument(userAuth);
+                userRef.onSnapshot(snapshot => {
+                    this.setState({
+                        currentUser:{
+                            id: snapshot.id,
+                            ...snapshot.data()
+                        }
+                    });
+                    console.log(this.state);
+                });
+            }else
+                {
+                this.setState({currentUser: userAuth});
+                }
 
-            console.log(user);
-        })
+        });
     }
     componentWillUnmount() {
         this.unsubscribefromAuth();
