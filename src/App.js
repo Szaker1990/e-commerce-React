d86523af913e6d76519cf0dfebdcf87203f6e904
szaker1990 <szaker1990@github.com>
2020-08-25 11:21:31 +0200

    form and sign in component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Homepage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.components";
 import Header from "./components/header/header.component";
+import SignInSignUp from "./pages/sign-in/sign-in-sign-up.component";
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
             <Switch>
                 <Route exact path="/" component={Homepage}/>
                 <Route path={"/shop"} component={ShopPage}/>
+                <Route path={"/signin"} component={SignInSignUp}/>
             </Switch>
         </div>
     );
---
+++ src/pages/sign-in/sign-in-sign-up.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./sign-in-sign-up.styles.scss"
+import SignIn from "../../components/sign-in/sign-in.components";
+
+const SignInSignUp = () => (
+    <div className={"sign-in-and-sign-up"}>
+        <SignIn/>
+    </div>
+)
+export default SignInSignUp
