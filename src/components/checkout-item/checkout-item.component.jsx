26d60bee2f5ba13e13e58007b9c408d871062251
szaker1990 <szaker1990@github.com>
2020-09-02 10:59:20 +0200

    styled components as Checkout Item

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,27 +1,28 @@
 import React from "react";
 import {connect} from "react-redux";
 import {clearItemFromCart, addItem, removeItem} from "../../redux/cart/cart.actions";
-
-import "./checkout-item.style.scss";
+import {CheckoutContainer, ImageContainer, ProductImage, NameSpan,
+    QuantitySpan,ArrowContainer,ValueSpan,
+    RemoveButtonContainer} from "./checkout-item.style";
 
 const CheckoutItem = ({cartItem, clearItem, addItem, removeItem}) => {
     const {name, imageUrl, price, quantity} = cartItem
     return (
-        <div className={"checkout-item"}>
-            <div className={"image-container"}>
-                <img src={imageUrl} alt={"item"}/>
-            </div>
-            <span className={"name"}>{name}</span>
-            <span className={"quantity"}>
-                <div className={"arrow"} onClick={() => removeItem(cartItem)}>&#10094;</div>
-                <span className={"value"}>
+        <CheckoutContainer>
+            <ImageContainer>
+                <ProductImage src={imageUrl} alt={"item"}/>
+            </ImageContainer>
+            <NameSpan>{name}</NameSpan>
+            <QuantitySpan>
+                <ArrowContainer className={"arrow"} onClick={() => removeItem(cartItem)}>&#10094;</ArrowContainer>
+                <ValueSpan>
                     {quantity}
-                </span>
-                <div className={"arrow"} onClick={() => addItem(cartItem)}>&#10095;</div>
-            </span>
-            <span className={"price"}>${price}</span>
-            <div onClick={() => clearItem(cartItem)} className={"remove-button"}>&#10005;</div>
-        </div>
+                </ValueSpan>
+                <ArrowContainer onClick={() => addItem(cartItem)}>&#10095;</ArrowContainer>
+            </QuantitySpan>
+            <NameSpan >${price}</NameSpan>
+            <RemoveButtonContainer onClick={() => clearItem(cartItem)}>&#10005;</RemoveButtonContainer>
+        </CheckoutContainer>
     )
 }
 const mapDispatchToProps = dispatch => ({
---
+++ src/components/checkout-item/checkout-item.style.jsx
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+export const CheckoutContainer = styled.div`
+  width: 100%;
+  display: flex;
+  min-height: 100px;
+  border-bottom: 1px solid darkgrey;
+  padding: 15px 0;
+  font-size: 20px;
+  align-items: center;
+`;
+export const ImageContainer = styled.div`
+  width: 23%;
+  padding-right: 15px;
+`;
+export const ProductImage = styled.img`
+  width: 100%;
+  height: 100%;
+`
+export const NameSpan = styled.span`
+  width: 23%;
+`;
+export const QuantitySpan = styled.span`
+  width: 23%;
+  display: flex;
+`;
+export const ArrowContainer = styled.div`
+  cursor:pointer;
+`;
+export const ValueSpan = styled.span`
+  margin: 0 10px;
+`;
+export const RemoveButtonContainer = styled.div`
+padding-left: 12px;
+cursor: pointer;
+`
